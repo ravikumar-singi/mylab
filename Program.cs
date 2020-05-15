002f154af2a67b5dc09e4e8a1bcb5e41d52d9a63
ravikumar-singi <ravikumar.singi@gmail.com>
2020-05-15 10:37:53 +0530

    First C# code assignment

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace mylab
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //1. TwoSum Problem -- Easy Leetcode
+            // int[] nums = new int[] { 2, 7, 11, 15 };
+            // int target = 19;
+            // var result = findTwoSum(nums, target);
+            // for (int i = 0; i < result.Length; i++)
+            // {
+            //     Console.WriteLine(result[i]);
+            // }
+            // Console.ReadLine();
+        }
+    }
+}
