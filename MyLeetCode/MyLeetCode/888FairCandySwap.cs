6cc4d2c31d7b34cb505b2a79a36b75d1bfdc2276
ravikumar-singi <ravikumar.singi@gmail.com>
2020-07-24 22:28:13 +0530

    today chages

--- MyLeetCode/MyLeetCode/888FairCandySwap.cs
+++ MyLeetCode/MyLeetCode/888FairCandySwap.cs
@@ -10,6 +10,31 @@ namespace Leetcode
         //     int[] B = new int[] { 2, 2 };
         //     FairCandySwap(A, B);
         // }
+        public int DaysBetweenDates(string date1, string date2)
+        {
+            System.DateTime dt1 = System.Convert.ToDateTime(date1);
+            System.DateTime dt2 = System.Convert.ToDateTime(date2);
+            return System.Math.Abs((int)(dt2 - dt1).TotalDays);
+        }
+
+        //   public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        //     int rows = binaryMatrix.Dimensions()[0];
+        //     int cols = binaryMatrix.Dimensions()[1];
+        //      // Set pointers to the top-right corner.
+        //     int currentRow = 0;
+        //     int currentCol = cols - 1;
+        //      // Repeat the search until it goes off the grid.
+        //     while (currentRow < rows && currentCol >= 0) {
+        //       if (binaryMatrix.get(currentRow, currentCol) == 0) {
+        //         currentRow++;
+        //       } else {
+        //         currentCol--; 
+        //       }
+        //     }
+        //     // If we never left the last column, this is because it was all 0's.
+        //     return (currentCol == cols - 1) ? -1 : currentCol + 1;
+        //   }
+
 
         public static int[] FairCandySwap(int[] A, int[] B)
         {
